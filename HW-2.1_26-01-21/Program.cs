19364ba845c1722b4e227fbcbe9bdf7fa5028ec0
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-01-29 17:42:24 +0500

    Modifier \  Checking numbers for valid

--- HW-2.1_26-01-21/Program.cs
+++ HW-2.1_26-01-21/Program.cs
@@ -8,42 +8,59 @@ namespace HW_2._1_26_01_21
         {
             Converter changeCurr = new Converter(11.3001, 13.6854, 0.1474);
             Console.WriteLine("Введите сумму: ");
-            int SUM = int.Parse(Console.ReadLine());
-            Console.WriteLine($"\tКурсы валют на сегодня: \n\t1 USD = {11.3001} TJS\n\t1 EUR = {13.6854} TJS \n\t1 RUB = {0.1474} TJS");
-            Console.WriteLine("\nПожалуйста, выберите тип конвертации:");
-            Console.WriteLine("Для конвертации TJS в USD нажмите - 1");
-            Console.WriteLine("Для конвертация TJS в EUR нажмите - 2");
-            Console.WriteLine("Для конвертация TJS в RUB нажмите - 3");
-            Console.WriteLine("Для конвертация USD в TJS нажмите - 4");
-            Console.WriteLine("Для конвертация EUR в TJS нажмите - 5");
-            Console.WriteLine("Для конвертация RUB в TJS нажмите - 6");
-            Console.WriteLine("\nВаш выбор: ");
-            int Choice = int.Parse(Console.ReadLine());
-
-            switch (Choice)
+            var isNumber = int.TryParse(Console.ReadLine(), out var SUM);
+            if (isNumber)
             {
-                case 1:
-                    Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToUsd(SUM)} USD");
-                    break;
-                case 2:
-                    Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToEur(SUM)} EUR");
-                    break;
-                case 3:
-                    Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToRub(SUM)} RUB");
-                    break;
-                case 4:
-                    Console.WriteLine($"{SUM} USD = {changeCurr.usdToTjs(SUM)} TJS");
-                    break;
-                case 5:
-                    Console.WriteLine($"{SUM} EUR ={changeCurr.eurToTjs(SUM)} TJS");
-                    break;
-                case 6:
-                    Console.WriteLine($"{SUM} RUB = {changeCurr.rubToTjs(SUM)} TJS");
-                    break;
-                default:
-                    Console.WriteLine("Неправильная команда. Пожалуйста, попробуйте еще раз");
-                    break;
+                if(SUM>0)
+                {
+                    Console.WriteLine($"\tКурсы валют на сегодня: \n\t1 USD = {11.3001} TJS\n\t1 EUR = {13.6854} TJS \n\t1 RUB = {0.1474} TJS");
+                    Console.WriteLine("\nПожалуйста, выберите тип конвертации:");
+                    Console.WriteLine("Для конвертации TJS в USD нажмите - 1");
+                    Console.WriteLine("Для конвертация TJS в EUR нажмите - 2");
+                    Console.WriteLine("Для конвертация TJS в RUB нажмите - 3");
+                    Console.WriteLine("Для конвертация USD в TJS нажмите - 4");
+                    Console.WriteLine("Для конвертация EUR в TJS нажмите - 5");
+                    Console.WriteLine("Для конвертация RUB в TJS нажмите - 6");
+                    Console.WriteLine("\nВаш выбор: ");
+                    int Choice = int.Parse(Console.ReadLine());
+
+                    switch (Choice)
+                    {
+                        case 1:
+                            Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToUsd(SUM)} USD");
+                            break;
+                        case 2:
+                            Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToEur(SUM)} EUR");
+                            break;
+                        case 3:
+                            Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToRub(SUM)} RUB");
+                            break;
+                        case 4:
+                            Console.WriteLine($"{SUM} USD = {changeCurr.usdToTjs(SUM)} TJS");
+                            break;
+                        case 5:
+                            Console.WriteLine($"{SUM} EUR ={changeCurr.eurToTjs(SUM)} TJS");
+                            break;
+                        case 6:
+                            Console.WriteLine($"{SUM} RUB = {changeCurr.rubToTjs(SUM)} TJS");
+                            break;
+                        default:
+                            Console.WriteLine("Неправильная команда. Пожалуйста, попробуйте еще раз");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Сумма меньше нулья");
+                }
             }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Сумма дольжен быть цифры!");
+            }
+           
             Console.ReadKey();
         }
     }
