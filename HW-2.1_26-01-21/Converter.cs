c22d9b71dd714b67d93e26e15414565c80b7a9e1
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-01-29 11:36:52 +0500

    Добавьте файлы проекта.

---
+++ HW-2.1_26-01-21/Converter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_2._1_26_01_21
+{
+    class Converter
+    {
+        private double usd { get; set; }
+        private double eur { get; set; }
+        private double rub { get; set; }
+        public Converter(double USD, double EUR, double RUB)
+        {
+            usd = USD;
+            eur = EUR;
+            rub = RUB;
+        }
+        public double tjsToUsd(double tjs)
+        {
+            return Math.Round(tjs / usd, 4);
+        }
+        public double tjsToEur(double tjs)
+        {
+            return Math.Round(tjs / eur, 4);
+        }
+        public double tjsToRub(double tjs)
+        {
+            return Math.Round(tjs / rub, 4);
+        }
+        public double usdToTjs(double USD)
+        {
+            return Math.Round(USD * usd, 4);
+        }
+        public double eurToTjs(double EUR)
+        {
+            return Math.Round(EUR * eur, 4);
+        }
+        public double rubToTjs(double RUB)
+        {
+            return Math.Round(RUB * rub, 4);
+        }
+    }
+}
---
+++ HW-2.1_26-01-21/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HW_2._1_26_01_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter changeCurr = new Converter(11.3001, 13.6854, 0.1474);
+            Console.WriteLine("Введите сумму: ");
+            int SUM = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\tКурсы валют на сегодня: \n\t1 USD = {11.3001} TJS\n\t1 EUR = {13.6854} TJS \n\t1 RUB = {0.1474} TJS");
+            Console.WriteLine("\nПожалуйста, выберите тип конвертации:");
+            Console.WriteLine("Для конвертации TJS в USD нажмите - 1");
+            Console.WriteLine("Для конвертация TJS в EUR нажмите - 2");
+            Console.WriteLine("Для конвертация TJS в RUB нажмите - 3");
+            Console.WriteLine("Для конвертация USD в TJS нажмите - 4");
+            Console.WriteLine("Для конвертация EUR в TJS нажмите - 5");
+            Console.WriteLine("Для конвертация RUB в TJS нажмите - 6");
+            Console.WriteLine("\nВаш выбор: ");
+            int Choice = int.Parse(Console.ReadLine());
+
+            switch (Choice)
+            {
+                case 1:
+                    Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToUsd(SUM)} USD");
+                    break;
+                case 2:
+                    Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToEur(SUM)} EUR");
+                    break;
+                case 3:
+                    Console.WriteLine($"{SUM} TJS = {changeCurr.tjsToRub(SUM)} RUB");
+                    break;
+                case 4:
+                    Console.WriteLine($"{SUM} USD = {changeCurr.usdToTjs(SUM)} TJS");
+                    break;
+                case 5:
+                    Console.WriteLine($"{SUM} EUR ={changeCurr.eurToTjs(SUM)} TJS");
+                    break;
+                case 6:
+                    Console.WriteLine($"{SUM} RUB = {changeCurr.rubToTjs(SUM)} TJS");
+                    break;
+                default:
+                    Console.WriteLine("Неправильная команда. Пожалуйста, попробуйте еще раз");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
